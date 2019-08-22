98a591773e45a2be77778685e15091b846b51467
Yolonaise <arnaud.schaal9@gmail.com>
2019-08-22 07:19:51 +0200

    renderizing the home and the menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/services/app.service';
+
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'liege-with-buddies';
-
+  
   constructor(private appService: AppService) {
+  }
+
+  ngOnInit() {
+    AOS.init();
   }
 }
