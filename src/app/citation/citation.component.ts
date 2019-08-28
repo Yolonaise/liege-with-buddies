d16d6eea6a408130117d410b01ce32dd60ab3fbd
asch <a.schaal@evs.com>
2019-08-28 10:16:52 +0200

    remove parallax effect on all element exept home children

--- src/app/citation/citation.component.ts
+++ src/app/citation/citation.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { IMenu } from 'src/Interface/menu.interface';
+
+import * as uuid from 'uuid';
+import { AppService } from 'src/services/app.service';
 
 @Component({
   selector: 'app-citation',
   templateUrl: './citation.component.html',
   styleUrls: ['./citation.component.scss']
 })
-export class CitationComponent implements OnInit {
+export class CitationComponent implements OnInit, IMenu {
+  htmlId: string = uuid.v4();;
+  name: string = "Liege";
 
-  constructor() { }
+  constructor(private appService: AppService) { }
 
   ngOnInit() {
+    this.appService.addFeature(this);
   }
-
 }
